c8c780d455cd6dbe93a6c7d6d92890fb7d1a4abe
mkierc <urban.pl@gmail.com>
2015-07-24 18:58:25 +0200

    Initial commit, backed up non-polish languages, added miasteczko studenckie as a location

---
+++ spyplok/lib/locations.js
@@ -0,0 +1,16 @@
+locations = [
+  {
+    name: 'locations.loc1',
+    roles: [
+      'locations.roles.loc1.role1',
+      'locations.roles.loc1.role2',
+      'locations.roles.loc1.role3',
+      'locations.roles.loc1.role4',
+      'locations.roles.loc1.role5',
+      'locations.roles.loc1.role6',
+      'locations.roles.loc1.role7',
+      'locations.roles.loc1.role8'
+    ]
+  }
+]
+
