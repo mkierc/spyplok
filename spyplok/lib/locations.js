40862230be1bd8c8491c8ce651d387a1ecdbc921
mkierc <urban.pl@gmail.com>
2015-07-24 19:21:28 +0200

    Added locations: Zakład Mechaniczny Cytryn & Gumiak, Student Market

--- spyplok/lib/locations.js
+++ spyplok/lib/locations.js
@@ -11,6 +11,28 @@ locations = [
       'locations.roles.loc1.role7',
       'locations.roles.loc1.role8'
     ]
+  },
+  {
+    name: 'locations.loc2',
+    roles: [
+      'locations.roles.loc2.role1',
+      'locations.roles.loc2.role2',
+      'locations.roles.loc2.role3',
+      'locations.roles.loc2.role4',
+      'locations.roles.loc2.role5',
+      'locations.roles.loc2.role6'
+	  ]
+  },
+  {
+    name: 'locations.loc3',
+    roles: [
+      'locations.roles.loc3.role1',
+      'locations.roles.loc3.role2',
+      'locations.roles.loc3.role3',
+      'locations.roles.loc3.role4',
+      'locations.roles.loc3.role5',
+      'locations.roles.loc3.role6'
+    ]
   }
 ]
 
